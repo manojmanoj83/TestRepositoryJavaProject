903deeca792aac003dd92f127cfd637d9558d388
Manoj <manojmali83@gmail.com>
2021-01-30 17:08:04 +0530

    Test Project

---
+++ MyClass/src/TestPackage/MyClass.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Manoj Is Working on Project");
+	}
+
+}
---
+++ MyClass/src/TestPackage/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author manoj_razf9u4
+ *
+ */
+package TestPackage;
