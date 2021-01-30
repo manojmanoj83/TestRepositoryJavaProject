497b75b5d11d08c080506f71d4978008e13edb70
Manoj <manojmali83@gmail.com>
2021-01-30 18:53:36 +0530

    Manoj Updated

--- MyClass/src/TestPackage/MyClass.java
+++ MyClass/src/TestPackage/MyClass.java
@@ -1,11 +1,26 @@
 package TestPackage;
 
-public class MyClass {
+public class MyClass 
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("Manoj Is Working on Project");
-     System.out.println("Manoj Is Working on Project");
+		
+		      /* Create two objects using constructor */
+		      Employee empOne = new Employee("James Smith");
+		      Employee empTwo = new Employee("Mary Anne");
+
+		      // Invoking methods for each object created
+		      empOne.empAge(26);
+		      empOne.empDesignation("Senior Software Engineer");
+		      empOne.empSalary(1000);
+		      empOne.printEmployee();
+
+		      empTwo.empAge(21);
+		      empTwo.empDesignation("Software Engineer");
+		      empTwo.empSalary(500);
+		      empTwo.printEmployee();
+		
 	}
 
 }
